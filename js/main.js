3252c33392e303a0fd3f3cccef7a449d298e71ef
IndieForger <indieforger@gmail.com>
2016-04-14 09:40:18 +0100

    script cleanup

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
 jQuery(function($) {'use strict',
-
-	//Initiat WOW JS
 	new WOW().init();
 
-	
-	//goto top
 	$('.gototop').click(function(event) {
 		event.preventDefault();
 		$('html, body').animate({
 			scrollTop: $("body").offset().top
 		}, 500);
-	});	
-
-	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
+	});
 });
